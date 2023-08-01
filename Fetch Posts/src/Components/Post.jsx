2b1415d143bce482ebb0d6dafeaf699c46a5b9d1
server_42 <devbhattacharya42@gmail.com>
2023-08-01 10:54:35 +0530

    Fetch Posts Project Done

--- Fetch Posts/src/Components/Post.jsx
+++ Fetch Posts/src/Components/Post.jsx
@@ -1,12 +1,15 @@
 import React from "react";
-const Post = () => {
+import styles from './post.module.css'
+const Post = ({props}) => {
   return (
-    <div className="post">
-      <h1>
+    <div className={styles.container}>
+      <h1 className={styles.heading}>
         {/* Here show the title of the post  */}
+        {props.title}
       </h1>
-      <h3>
+      <h3 className={styles.body}>
         {/* Here show the body of the post  */}
+        {props.body}
       </h3>
     </div>
   );
